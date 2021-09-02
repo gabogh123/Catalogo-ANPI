5a50adacac07f67c56f7863a3ae7a780a54c106d
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-09-01 19:17:41 -0600

    [Update] Método Gradiente Conjugado No Lineal (funciona))

--- P2/conjugado.m
+++ P2/conjugado.m
@@ -1,33 +1,31 @@
 function conjugado()
   clc; clear; 
-  %Calcular el cero de exp(x)-2*x-10=0
-  %Paso 1: Conocer el intervalo donde 
-  %        se encuentra el cero
-  %        Sugerencia: graficar la funcion
-  %        f(x)=exp(x)-2*x-10
-  
-
-  
-  %Concluimos que la funcion tiene un cero 
-  %en el intervalo [2,4]
-
-
-  f = '(x-2)^4 + (x-2*y)^2';
-  x0 = [0 ;3];
-  tol = 0.0000001;
-  iterMax = 1000;
-  [xk k error] = gcln(f,x0,tol,iterMax)
+  f = '(x-2)^4 + (x-2*y)^2'; %String que representa la funcion a evaluar
+  x0 = [0 ;3]; %Vector con valores iniciales
+  tol = 0.0000001; %Valor de tolerancia aceptada
+  iterMax = 1000; %Valor máximo de iteraciones
+  [x_k k error] = gradiente_conjugado(f,x0,tol,iterMax)
 end
 
-function [xk k error] = gcln(f,x0,tol,iterMax)
+function [x_k k error] = gradiente_conjugado(f,x0,tol,iterMax)
+  %El método de gradiente conjugado no lineal permite resolver 
+  %problemas de optimización en variable variable, por ello, 
+  %en la función gradiente_conjugado
+  %se desea conocer un valor mínimo de la función.
+  %Parámetros de entrada:
+  %     f = función en estudio       
+  %     x0 = vector inicial
+  %     tol = Valor de tolerancia aceptada
+  %     iterMax = Cantidad máxima de iteraciones
+  
   pkg load symbolic;
   f1 = matlabFunction(sym (f))
   error = tol + 1;
   k = 1;
   x = sym('x');
   y = sym('y');
-  grad_f = gradient(f1, [x, y])
-  g_k = grad_f(x0);
+  grad_f = matlabFunction(gradient(f1,[x y])) %Se averigua la función gradiente de f(x,  y)
+  g_k = grad_f(x0(1), x0(2)); %Se evalúa el vector incial en el gradiente
   d_k = -1.0*g_k;
   x_k = x0;
   %Creacion de vectores para la grafica
@@ -35,12 +33,13 @@ function [xk k error] = gcln(f,x0,tol,iterMax)
   er = [];
   while k < iterMax
     
-    alpha_k = 1; %delta en ]0, 1[
-    sigma = 0.5;
+    alpha_k = 1; 
+    sigma = 0.5; %sigma en ]0, 1[
+    %Ciclo para calcular el tamaño de paso alpha_k
     for i = 1:iterMax
       arr = x_k + alpha_k*d_k;
-      izq = f(arr + alpha_k*d_k)-f(x_k); 
-      der = sigma*alpha_k*sum(g_k*d_k);
+      izq = f1(arr(1), arr(2)) - f1(x_k(1),x_k(2)); 
+      der = sigma*alpha_k*sum(g_k'*d_k);
       if izq < der
         break;
       else
@@ -49,7 +48,7 @@ function [xk k error] = gcln(f,x0,tol,iterMax)
     endfor
      
     x_k = x_k + alpha_k*d_k;
-    g_k1 = grad_f(x_k);
+    g_k1 = grad_f(x_k(1), x_k(2));
     error = norm(g_k1);
     iter = [iter;k]; %Se concatena la iteracion al vector de iteraciones
     er = [er;error]; %Se concatena el error al vector de errores
@@ -57,10 +56,10 @@ function [xk k error] = gcln(f,x0,tol,iterMax)
       break;
     endif 
     
-    beta = norm(g_k1).^2/norm(g_k).^2 
-    g_k1 = g_k1;
-    d_k = -1.0*g_k1 + d_k*beta
-    k = k + 1
+    beta = norm(g_k1)^2/norm(g_k)^2; %Selección del parámetro Beta según la regla de Fletcher y Reeves
+    g_k = g_k1;
+    d_k = -1.0*g_k1 + d_k*beta;
+    k = k + 1;
   endwhile
   plot(iter,er) %Se plotean las iteraciones vs error
   grid on; %Se muestra el grid
