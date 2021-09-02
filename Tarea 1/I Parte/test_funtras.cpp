4647bfd17458270a638471f9a151420911366139
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-09-02 12:08:27 -0600

    [Added] Primera Parte Tarea 1

---
+++ Tarea 1/I Parte/test_funtras.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "funtras.h"
+using namespace std;  
+int main()
+{
+    example();
+    //cpp_bin_float_100 pii = pi();
+    //cout << setprecision(10) << pii << endl;
+    return 0;
+}
